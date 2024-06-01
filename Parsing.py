a22e25b52f43db646d90ec2e2e4ba874be550425
Jakub-Be <107358283+Jakub-Be@users.noreply.github.com>
2024-06-01 19:01:39 +0200

    Add output file

--- Parsing.py
+++ Parsing.py
@@ -2,14 +2,18 @@ from urllib.parse import urlparse, parse_qs
 import pandas as pd
 
 input = pd.read_csv('task1_input.tsv', sep='\t')
-output = pd.DataFrame()
+rows = []
 
 for url in data['url']:
   parsed_url = urlparse(url)
   query_params = parse_qs(parsed_url.query)
 
-  row = {'URL': url}
-  row.update(query_params)
-  output = output._append(row, ignore_index=True)
+  row = {'url': url}
+  for key, value in query_params.items():
+    row[key] = value[0]
+  rows.append(row)
+
+output = pd.DataFrame(rows)
+output.to_csv('output.tsv', sep='\t', index=False)
 
 print(output)
