c1511bd212c9ffc72f8cf09686cf202fa168267b
Jakub-Be <107358283+Jakub-Be@users.noreply.github.com>
2024-06-01 18:15:44 +0200

    Update Parsing.py
    Analyze input data directly form file

--- Parsing.py
+++ Parsing.py
@@ -1,8 +1,9 @@
 from urllib.parse import urlparse, parse_qs
+from pandas import read_csv
 
-url = "https://www.tidio.com/?utm_source=source1&utm_medium=cpc&utm_campaign=999999999&utm_content=9999999utm_term=+fake_term+chat&a_bucket=bucket1&a_type=type1&a_source=source1&a_v=2&a_g_campaignid=999999999&a_g_keyword=+fake_term&a_g_adgroupid=999999999&a_g_creative=999999999"
+data = read_csv('task1_input.tsv', sep='\t')
 
-parsed_url = urlparse(url)
-query_params = parse_qs(parsed_url.query)
-
-print(query_params)
+for url in data['url']:
+  parsed_url = urlparse(url)
+  query_params = parse_qs(parsed_url.query)
+  print(f"Params: {query_params}")
