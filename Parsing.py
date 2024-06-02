1a9c12cd0b631d21ae0348d24d33720cc3b62777
Jakub-Be <107358283+Jakub-Be@users.noreply.github.com>
2024-06-02 12:43:19 +0200

    Add mapping dictionary and loop logic change

--- Parsing.py
+++ Parsing.py
@@ -1,19 +1,23 @@
 from urllib.parse import urlparse, parse_qs
 import pandas as pd
 
-input = pd.read_csv('task1_input.tsv', sep='\t')
-rows = []
+data = read_csv('task1_input.tsv', sep='\t')
 
-for url in data['url']:
-  parsed_url = urlparse(url)
-  query_params = parse_qs(parsed_url.query)
+mapping_dict = {
+    'a_bucket': 'ad_bucket',
+    'a_type': 'ad_type',
+    'a_source': 'ad_source',
+    'a_v': 'schema_version',
+    'a_g_campaignid': 'ad_campaign_id',
+    'a_g_keyword': 'ad_keyword',
+    'a_g_adgroupid': 'ad_adgroup_id', #should be ad_group_id?
+    'a_g_creative': 'ad_creative'
+}
 
-  row = {'url': url}
-  for key, value in query_params.items():
-    row[key] = value[0]
-  rows.append(row)
+for index, row in data.iterrows():
+    parsed_url = urlparse(row['url'])
+    query_params_dict = parse_qs(parsed_url.query)
+    for query_param_name, mapped_column in mapping_dict.items():
+        data.at[index, mapped_column] = query_params_dict.get(query_param_name, [''])[0]
 
-output = pd.DataFrame(rows)
-output.to_csv('output.tsv', sep='\t', index=False)
-
-print(output)
+data.to_csv('output.tsv', sep='\t', index=False)
